4b99d71d4fb65201ac3c8fe207e0b98a06f0b562
jeikerxiao <jeiker@126.com>
2019-06-03 17:05:54 +0800

    init: sofa-demo-xml

---
+++ service-facade/src/main/java/com/jeiker/boot/SampleJvmService.java
@@ -0,0 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jeiker.boot;
+
+/**
+ * @author xuanbei 18/5/5
+ * 用于演示 JVM 服务发布与引用的 API
+ */
+public interface SampleJvmService {
+    String message();
+}
---
+++ sofa-boot-run/src/main/java/com/jeiker/bootrun/SofaBootRunApplication.java
@@ -0,0 +1,13 @@
+package com.jeiker.bootrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootRunApplication.class, args);
+    }
+
+}
